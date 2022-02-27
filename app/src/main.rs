50f1dc97bbeac6ff903cf50cfc10990ecbf2a90e
Yan Jiang <jarodzz@gmail.com>
2022-02-27 13:58:12 +0800

    feature - switch to actix, compile work

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,159 +1,151 @@
-// Licensed to the Apache Software Foundation (ASF) under one
-// or more contributor license agreements.  See the NOTICE file
-// distributed with this work for additional information
-// regarding copyright ownership.  The ASF licenses this file
-// to you under the Apache License, Version 2.0 (the
-// "License"); you may not use this file except in compliance
-// with the License.  You may obtain a copy of the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied.  See the License for the
-// specific language governing permissions and limitations
-// under the License..
-
 extern crate sgx_types;
 extern crate sgx_urts;
 extern crate openssl;
 
+use std::sync::Mutex;
 use sgx_types::*;
 use sgx_urts::SgxEnclave;
 
-use futures_util::{SinkExt, StreamExt, TryFutureExt};
-use tokio::sync::{mpsc, RwLock};
-use tokio_stream::wrappers::UnboundedReceiverStream;
-use warp::Filter;
-use warp::ws::{Message, WebSocket};
-
-use openssl::rsa::{Rsa, Padding};
-use openssl::symm::Cipher;
+use serde_derive::{Deserialize, Serialize};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 static ENCLAVE_FILE: &'static str = "enclave.signed.so";
 
 extern {
-    fn register_key(eid: sgx_enclave_id_t, retval: *mut sgx_status_t,
+    fn exchange_key(eid: sgx_enclave_id_t, retval: *mut sgx_status_t,
                      some_string: *const u8, len: usize) -> sgx_status_t;
 }
 
-fn init_enclave() -> SgxResult<SgxEnclave> {
+fn init_enclave() -> SgxEnclave {
     let mut launch_token: sgx_launch_token_t = [0; 1024];
     let mut launch_token_updated: i32 = 0;
     // call sgx_create_enclave to initialize an enclave instance
     // Debug Support: set 2nd parameter to 1
     let debug = 1;
     let mut misc_attr = sgx_misc_attribute_t {secs_attr: sgx_attributes_t { flags:0, xfrm:0}, misc_select:0};
-    SgxEnclave::create(ENCLAVE_FILE,
+    let sgxResult = SgxEnclave::create(ENCLAVE_FILE,
                        debug,
                        &mut launch_token,
                        &mut launch_token_updated,
-                       &mut misc_attr)
-}
-
-
-async fn register_user(ws: WebSocket) {
-
-    println!("Websocket connected successfully!");
-
-    let enclave = match init_enclave() {
+                       &mut misc_attr);
+    match sgxResult {
         Ok(r) => {
             println!("[+] Init Enclave Successful {}!", r.geteid());
-            r
+            return r;
         },
         Err(x) => {
-            println!("[-] Init Enclave Failed {}!", x.as_str());
-            return;
+            panic!("[-] Init Enclave Failed {}!", x.as_str());
         },
     };
-
-    let (mut user_ws_tx, mut user_ws_rx) = ws.split();
-    let (tx, rx) = mpsc::unbounded_channel();
-    let mut rx = UnboundedReceiverStream::new(rx);
-
-    tokio::task::spawn(async move {
-        while let Some(message) = rx.next().await {
-            user_ws_tx
-                .send(message)
-                .unwrap_or_else(|e| {
-                    eprintln!("websocket send error: {}", e);
-                })
-                .await;
-        }
-    });
-
-    // Every time the user sends a message, broadcast it to
-    // all other users...
-    while let Some(result) = user_ws_rx.next().await {
-        let msg = match result {
-            Ok(msg) => msg,
-            Err(e) => {
-                eprintln!("websocket error(uid={}): {}", enclave.geteid(), e);
-                break;
-            }
-        };
-        match msg.to_str() {
-            Ok(strmsg) => {
-                eprintln!("getting msg in warp: {}", strmsg.to_string());
-                let rsa = Rsa::public_key_from_pem(strmsg.to_string().as_bytes()).unwrap();
-                user_message(enclave.geteid(), strmsg.to_string());
-            },
-            Err(e) => {
-                eprintln!("user send not a text msg");
-                break;
-            }
-        }
-    }
-    enclave.destroy();   
 }
 
-fn user_message(eid: sgx_enclave_id_t, msg: String) {
-    let mut retval = sgx_status_t::SGX_SUCCESS;
-
-    let result = unsafe {
-        register_key(eid,
-            &mut retval,
-            msg.as_ptr() as * const u8,
-            msg.len())
-    };
-    match result {
-        sgx_status_t::SGX_SUCCESS => {},
-        _ => {
-            println!("[-] ECALL Enclave Failed {}!", result.as_str());
-            return;
-        }
-    }
-
+async fn app_seal_key(safeMessage: &SafeMessage) {
+    println!("{}", safeMessage.secret);
 }
 
-#[tokio::main]
-async fn main() {
+async fn app_unseal_key(safeMessage: &SafeMessage) {
+    println!("{}", safeMessage.secret);
+}
+
+#[derive(Deserialize, Serialize)]
+struct SafeMessage {
+    secret: String
+}
+
+struct AppState {
+    enclave: SgxEnclave 
+}
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    // for health check
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/exchange_key/{user_pub_key}")]
+async fn app_exchange_key(
+    path: web::Path<(String,)>,
+    endex: web::Data<AppState>
+) -> impl Responder {
+    let e = &endex.enclave;
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/seal")]
+async fn seal(
+    req_body: String,
+    endex: web::Data<AppState>
+) -> impl Responder {
+    let e = &endex.enclave;
+    HttpResponse::Ok().body(req_body)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let edata = web::Data::new(Mutex::new(AppState{
+        enclave: init_enclave()
+    }));    
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::clone(&edata))
+            .service(app_exchange_key)
+            .service(seal)
+            .service(hello)
+    })
+    .bind("0.0.0.0:12345")?
+    .run()
+    .await
+}
+
+// #[tokio::main]
+// async fn main() {
+
+//     let enclave = match init_enclave() {
+//         Ok(r) => {
+//             println!("[+] Init Enclave Successful {}!", r.geteid());
+//             r
+//         },
+//         Err(x) => {
+//             println!("[-] Init Enclave Failed {}!", x.as_str());
+//             return;
+//         },
+//     };
+
+//     let exchange_key_url = warp::path("exchange_key")
+//         //.and(warp::path::param())
+//         .map(|| {
+//             //app_exchange_key(enclave.geteid(), param);
+//             warp::reply::html(INDEX_HTML);
+//         });
+
+//     let seal_key_url = warp::post()
+//         .and(warp::path("seal"))
+//         .and(warp::body::json())
+//         .map(|msg: SafeMessage| {
+//             app_seal_key(&msg);
+//             warp::reply::json(&msg);
+//         });
     
-    let register_url = warp::path("register")
-        .and(warp::ws())
-        .map(|ws: warp::ws::Ws| {
-            ws.on_upgrade(move |socket| register_user(socket))
-        });
+//     let unseal_key_url = warp::post()
+//         .and(warp::path("unseal"))
+//         .and(warp::body::json())
+//         .map(|msg: SafeMessage| {
+//             app_unseal_key(&msg);
+//             warp::reply::json(&msg);
+//         });
 
-    let index = warp::path::end().map(|| warp::reply::html(INDEX_HTML));
-    let routes = index.or(register_url);
+//     let index = warp::path::end().map(|| warp::reply::html(INDEX_HTML));
+    
+//     let routes = index
+//         .or(exchange_key_url)
+//         .or(seal_key_url)
+//         .or(unseal_key_url);
 
-    warp::serve(routes)
-        .tls()
-        .cert_path("certs/server.crt")
-        .key_path("certs/server.key")
-        .run(([0, 0, 0, 0], 12346))
-        .await;
-}
-
-static INDEX_HTML: &str = r#"<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title>Warp Health Check</title>
-    </head>
-    <body>
-        <h1>Warp is up and running.</h1>
-    </body>
-</html>
-"#;
+//     warp::serve(exchange_key_url)
+//         // .tls()
+//         // .cert_path("certs/server.crt")
+//         // .key_path("certs/server.key")
+//         .run(([0, 0, 0, 0], 12346))
+//         .await;
+// }
--- enclave/src/lib.rs
+++ enclave/src/lib.rs
@@ -38,33 +38,45 @@ use std::vec::Vec;
 use std::io::{self, Write};
 use std::slice;
 use std::str;
+use sgx_crypto_helper::RsaKeyPair;
+use sgx_crypto_helper::rsa3072::{Rsa3072KeyPair, Rsa3072PubKey};
+
+
+// generate right after sgx init, destroy after sgx exit
+// static KEY: Rsa3072KeyPair = Rsa3072KeyPair::new().unwrap();
+
 
 #[no_mangle]
-pub extern "C" fn register_key(some_string: *const u8, some_len: usize) -> sgx_status_t {
+pub extern "C" fn exchange_key(
+    user_in: *const u8, 
+    some_len: usize,
+    sgx_out: *mut u8) -> sgx_status_t {
+        // input user public key, output sgx public key
+        // let exported_pubkey: Rsa3072PubKey = rsa_keypair.export_pubkey().unwrap();
+        // let imported_pubkey: Rsa3072PubKey = serde_json::from_str(&serialized_pubkey).unwrap();
 
-    let str_slice = unsafe { slice::from_raw_parts(some_string, some_len) };
-    let key_str = String::from_utf8(str_slice.to_vec()).unwrap();
-    let key_parts: Vec<&str> = key_str.split("\n").collect();
-    println!("enclave: {}", key_parts[1]);
-    let key: &[u8] = key_parts[1].as_bytes();
-    let n: [u8; 256] = [0; 256];
-    let pubkey = SgxRsaPubKey::new();
-    pubkey.create(256, 4, key, &[0x00, 0x01, 0x00, 0x01]);
+    // let str_slice = unsafe { slice::from_raw_parts(some_string, some_len) };
+    // let key_str = String::from_utf8(str_slice.to_vec()).unwrap();
+    // let key_parts: Vec<&str> = key_str.split("\n").collect();
+    // println!("enclave: {}", key_parts[1]);
+    // let key: &[u8] = key_parts[1].as_bytes();
+    // let n: [u8; 256] = [0; 256];
+    // let pubkey = SgxRsaPubKey::new();
+    // pubkey.create(256, 4, key, &[0x00, 0x01, 0x00, 0x01]);
 
-    let text = String::from("Hello, World.");
-    let text_slice = &text.into_bytes();
-    let mut ciphertext: Vec<u8> = vec![0_u8; 256];
-    let mut chipertext_len: usize = ciphertext.len();
-    match pubkey.encrypt_sha256(
-        ciphertext.as_mut_slice(), 
-        &mut chipertext_len,
-        text_slice
-    ) {
-        Ok(n) => println!("Generated secret with user pub key, {}", ciphertext),
-        Err(x) => println!("Error occurred during encryption {}", x)
-    }
-
-    sgx_status_t::SGX_SUCCESS
+    // let text = String::from("Hello, World.");
+    // let text_slice = &text.into_bytes();
+    // let mut ciphertext: Vec<u8> = vec![0_u8; 256];
+    // let mut chipertext_len: usize = ciphertext.len();
+    // match pubkey.encrypt_sha256(
+    //     ciphertext.as_mut_slice(), 
+    //     &mut chipertext_len,
+    //     text_slice
+    // ) {
+    //     Ok(n) => println!("Generated secret with user pub key, {}", ciphertext),
+    //     Err(x) => println!("Error occurred during encryption {}", x)
+    // }
+    return sgx_status_t::SGX_SUCCESS
 }
 
 fn clone_into_array(aslice: &[u8]) -> [u8; 256] {
@@ -75,4 +87,11 @@ fn clone_into_array(aslice: &[u8]) -> [u8; 256] {
         i = i + 1;
     }
     return arr;
+}
+
+pub extern "C" fn register_key(user_in: *const u8, 
+    some_len: usize,
+    sgx_out: *mut u8) -> sgx_status_t {
+        // decrypt user input, and seal it.
+        return sgx_status_t::SGX_SUCCESS
 }
