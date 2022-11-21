b653e56915573c8ecb55f1b7afeb23692e085886
Yan Jiang <jarodzz@gmail.com>
2022-11-21 13:08:43 +0800

    feat: add docker file for building docker image

---
+++ docker/get_sgx_ssl.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source ${SGX_SDK}/environment
+git clone https://github.com/intel/intel-sgx-ssl.git
+wget https://www.openssl.org/source/old/1.1.1/openssl-1.1.1q.tar.gz
+mv openssl-1.1.1q.tar.gz intel-sgx-ssl/openssl_source
+cd intel-sgx-ssl/Linux
+make all test SGX_MODE=SIM 
+make install 
